67cdfed544175e5e825462d5f46a41916e999776
Favour04 <idaeworfavour1@gmail.com>
2024-07-25 18:43:14 +0100

    Created a magnificent trianle (pascala_t)

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,24 +2,28 @@
 """
 Get factorial of a number
 """
-def factorial(num):
-        if num == 0:
-             return 1
-        
-        if num <= 1:
-            return num
-        else:
-            return num * factorial(num - 1)
 
-"""
-generate pascal tiangle
-"""
+
+def f(num):
+    """
+        This fuction is used to get the factorial of a number
+    """
+    if num == 0:
+        return 1
+
+    if num <= 1:
+        return num
+    else:
+        return num * f(num - 1)
+
+
 def pascal_triangle(num):
     """
-    The function generate a pascal triangle
+        The function generate a pascal triangle
     """
-     pascals = []
-     for i in range(num + 1):
-          fac = list(map(lambda n: int(factorial(i) / (factorial(i - n) * factorial(n))), range(i + 1)))
-          pascals.append(fac)
-     return pascals
+    pascals = []
+    for i in range(num + 1):
+        fac = list(map(lambda n: int(f(i) / (f(i - n) * f(n))), range(i + 1)))
+        pascals.append(fac)
+
+    return pascals
